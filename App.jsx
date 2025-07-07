689ab221bd17957e35a0e58baefd37ea51763a47
mosalih97 <m.el3min3@gmail.com>
2025-07-07 09:17:11 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>مرحبا بك في الكرين</h1>
+      <p>تطبيق بيع وشراء السيارات السوداني</p>
+    </div>
+  )
+}
+
+export default App
