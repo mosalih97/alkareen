6f536d9f94a2287bf8daff468c706d822bc65e1f
mosalih97 <m.el3min3@gmail.com>
2025-07-07 08:21:01 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <h1 style={{ textAlign: "center", marginTop: "50px" }}>مرحبًا بك في الكرين 🚗</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
